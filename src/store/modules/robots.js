3d92098950db088966e813fdacbb12b8ea55db35
faround <pillowsandskies@gmail.com>
2020-01-28 16:09:13 +0200

    Revert "finished 54!"
    This reverts commit e7ce58bb

--- src/store/modules/robots.js
+++ src/store/modules/robots.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default{
-    namespaced:true,
 //adding data to the store using mutations
     state:{
         cart:[],/*important to add a default value to everything that is added to the store.*/
