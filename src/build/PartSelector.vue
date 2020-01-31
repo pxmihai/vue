16e59bbbdb59f6498101e6d61bfad8d12d9a30b5
faround <pillowsandskies@gmail.com>
2020-01-31 09:27:49 +0200

    working up to 65

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -12,11 +12,14 @@
 <!--        <img  @click="showPartInfo()" :src="selectedPart.src" title="arm"/>-->
         <button @click="selectPreviousPart()" class="prev-selector"></button>
         <button @click="selectNextPart()" class="next-selector"></button>
-        <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+        <span v-pin="{bottom:'10px',right:'50px'}" class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <!--<span v-pin:position.top.right-->
+    <!-- position is the arg and top right the modifiers-->
     </div>
 </template>
 
 <script>
+    import pinDirective from '../shared/pin-directive';
     // import availableParts from '../data/parts';
     // const parts = availableParts.heads;
 
@@ -31,6 +34,11 @@
     }
 
     export default {
+        directives:{
+            pin:pinDirective,
+        // we use v-pin because pin is the name of the directive created
+
+        },
         // props:['parts', 'position'],
         props:{
             parts: {
@@ -98,9 +106,9 @@
         border: 3px solid #aaa;
     }
     .sale {
-        position: absolute;
-        bottom: 5px;
-        right: 5px;
+        /*position: absolute;*/
+        /*bottom: 5px;*/
+        /*right: 5px;*/
         color: white;
         background-color: red;
         padding: 3px;
