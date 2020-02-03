5c645e277e90af75e010cc7c6991c5ade117b6d6
faround <pillowsandskies@gmail.com>
2020-02-03 19:02:14 +0200

    final commit, project completed 77/77

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,28 @@
 module.exports={
+    // configureWebpack:{
+    configureWebpack:(config)=>{
+        config.module.rules.push({
+                test:/\.coffee$/,
+                use: ['coffee-loader'],
+    });
+        const newRule=  {
+
+            test:/\.(png|jpe?g|gif)(\?.*)?$/,
+            use:[
+                {
+                    loader:'url-loader',
+                    options:{
+                        limit:5000,
+                        name:'img/[name].[hash:8].ext]',
+                    },
+                },
+            ],
+        };
+    const imagesRuleIndex = config.module.rules.findIndex( x=>x.test.source.includes('png|jpe?g|gif'));
+    //typo in module.rules module breaks the code, no warnings
+
+    config.module.rules.splice(imagesRuleIndex,1,newRule);
+    },
     devServer:{
         proxy:{
             '/api/':{
